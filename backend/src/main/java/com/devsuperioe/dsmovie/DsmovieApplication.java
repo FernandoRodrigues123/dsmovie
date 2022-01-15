41c39874e744e8290fc494ba3c2b2fdf30eaafa9
Fernando Rodrigues123 <fernandorifpro@gmail.com>
2022-01-15 14:00:35 -0300

    error

--- backend/src/main/java/com/devsuperioe/dsmovie/DsmovieApplication.java
+++ backend/src/main/java/com/devsuperioe/dsmovie/DsmovieApplication.java
@@ -1,12 +1,14 @@
 package com.devsuperioe.dsmovie;
 
+import javax.persistence.EntityManager;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DsmovieApplication {
+public class DsmovieApplication  {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		SpringApplication.run(DsmovieApplication.class, args);
 	}
 
