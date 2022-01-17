4c8036e819e7fdde9cfc93555cec2506e18ff66d
Fernando Rodrigues123 <fernandorifpro@gmail.com>
2022-01-16 21:19:05 -0300

    alteracao de correcao

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -5,7 +5,6 @@ import{ReactComponent as StarHalf} from 'assets/img/star-half.svg';
 import{ReactComponent as StarEmpty} from 'assets/img/star-empty.svg';
 
 import'./styles.css';
-import { type } from 'os';
 
 type Props = {
   score: number;
